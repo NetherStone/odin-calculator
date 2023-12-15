d841f45029ae4497fdb8da5ee81fd5e6db07fb0e
Regan <reganpuah@gmail.com>
2023-12-15 10:40:06 +0800

    Created function that calls the previous functions. Function takes 2 numbers and 1 operator.

--- js-file.js
+++ js-file.js
@@ -6,6 +6,21 @@ const Multiplication = (a, b) => a * b; //Multiplication
 
 const Division = (a, b) => a / b; //Division
 
+function operate (firstNumber, secondNumber, operator) {
+    if (operator === "+") {
+        return Addition(firstNumber, secondNumber)
+    }
+    if (operator === "-") {
+        return Subtraction(firstNumber, secondNumber)
+    }
+    if (operator === "*") {
+        return Multiplication(firstNumber, secondNumber)
+    }
+    if (operator === "/") {
+        return Division(firstNumber, secondNumber)
+    }
+}
+
 let firstNumber = 0; //Initialize variables
 let secondNumber = 0;
 let operator = "";
